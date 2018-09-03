1f2f67a4ecf04199ee3e4a58ebf477ffeeb496c0
nikos <nikos@mugsoft.io>
2018-09-03 04:31:55 +0400

    middleware support and set cache middleware

--- delivery/api/init.go
+++ delivery/api/init.go
@@ -11,16 +11,47 @@ import (
 //Mount root handler
 func Mount() http.Handler {
 	var mux = httprouter.New()
+	//__mux is the interface so it can be used for middlewares
+	var __mux http.Handler
 	mount__user(mux)
-	return mux
+	__mux = __middleware_headers_set(mux)
+	return __mux
 }
 
 func __fv(r *http.Request, field__name string) string {
 	return r.FormValue(field__name)
 }
 
+func __middleware_headers_set(next http.Handler) http.Handler {
+	__hdrs := map[string]string{
+		"Content-Type":                "application/json",
+		"Access-Control-Allow-Origin": "*",
+	}
+
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		for k, v := range __hdrs {
+			w.Header().Set(k, v)
+		}
+		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
+
 func __parse__form(w http.ResponseWriter, r *http.Request) error {
 	err := r.ParseForm()
+	if nil != err {
+		helpers.Log(helpers.ERR, err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return err
+	}
+	return nil
+}
+
+func __parse__multipart__form(w http.ResponseWriter, r *http.Request, size int64) error {
+	err := r.ParseMultipartForm(size)
 	if nil != err {
 		helpers.Log(helpers.ERR, err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
