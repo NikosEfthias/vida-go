bc0546939e7f5120aa2485198fecedfcd2d0ddf6
nikos <nikos.efthias@gmail.com>
2018-09-11 04:01:25 +0400

    use more reasonable fields (computed ones) and add parsing options

--- models/event.go
+++ models/event.go
@@ -12,19 +12,20 @@ const __COL_EVENT_STR = "events"
 var __col_event = db__get().C(__COL_EVENT_STR)
 
 type Event struct {
-	Id        string
-	Owner     string
-	Invitees  string //i1_mail;i1_phone:i2_mail;i2_phone syntax for compatibility with other dbs without owerhead this wont be used to search just for visual purposes
-	Title     string
-	Loc       string
-	Detail    string
-	MaxGuest  int
-	MinGuest  int
-	Cost      float64
-	Img       string
-	StartDate time.Time
-	EndDate   time.Time
-	Defaults
+	Id        string        `json:"id" bson:"id"`
+	Owner     string        `json:"owner" bson:"owner"`
+	Invitees  []*Invitation `json:"invitees" bson:"-"` //i1_mail;i1_phone:i2_mail;i2_phone syntax for compatibility with other dbs without owerhead this wont be used to search just for visual purposes
+	Title     string        `json:"title" bson:"title"`
+	Loc       string        `json:"loc" bson:"loc"`
+	Detail    string        `json:"detail" bson:"detail"`
+	MaxGuest  int           `json:"max_guest" bson:"max_guest"`
+	MinGuest  int           `json:"min_guest" bson:"min_guest"`
+	Cost      float64       `json:"cost" bson:"cost"`
+	Img       string        `json:"img" bson:"img"`
+	EventType int           `json:"event_type" bson:"event_type"` //1:static single date 2: votable single date (date range )
+	StartDate time.Time     `json:"start_date" bson:"start_date"`
+	EndDate   time.Time     `json:"end_date" bson:"end_date"`
+	Defaults  `json:"defaults" bson:"defaults"`
 }
 
 func Event_new(e *Event) error {
--- models/invitation.go
+++ models/invitation.go
@@ -1,12 +1,13 @@
 package models
 
 type Invitation struct {
-	Id        string
-	EventId   string
-	Status    int //0:pending,1:accepted,2:declined
-	Message   string
-	Type      int //0:event,1:app invitation
-	InviterId string
-	InviteeId string //if invitee is already member
-	Defaults
+	Id        string `json:"-" bson:"id,omitempty"`
+	EventId   string `json:"-" bson:"event_id,omitempty"`
+	Status    int    `json:"status,omitempty" bson:"status,omitempty"` //0:pending,1:accepted,2:declined
+	Message   string `json:"message,omitempty" bson:"message,omitempty"`
+	Type      int    `json:"type,omitempty" bson:"type,omitempty"` //0:event,1:app invitation
+	InviterId string `json:"inviter_id,omitempty" bson:"inviter_id,omitempty"`
+	InviteeId string `json:"invitee_id,omitempty" bson:"invitee_id,omitempty"` //if invitee is already member
+	Invitee   *User  `json:"invitee,omitempty" bson:"-"`
+	Defaults  `json:"defaults,omitempty" bson:"defaults,omitempty"`
 }
