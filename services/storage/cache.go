f3c159f679032a997960a24d99249785afd3f8b3
nikos <nikos@mugsoft.io>
2018-11-18 03:17:26 +0400

    fix underscore styles

--- services/storage/cache.go
+++ services/storage/cache.go
@@ -8,26 +8,28 @@ import (
 	"github.com/mugsoft/vida/models"
 )
 
-var __cache__user = struct {
+var _cache_user = struct {
 	sync.Mutex
-	by__id    map[string]*models.User
-	by__token map[string]*models.User
+	by_id    map[string]*models.User
+	by_token map[string]*models.User
 }{
+	//{{{
 	sync.Mutex{},
 	map[string]*models.User{},
 	map[string]*models.User{},
+	//}}}
 }
 
 func Get_user_by_id(k string) *models.User {
-	__cache__user.Lock()
-	defer __cache__user.Unlock()
-	return __is__expired(__cache__user.by__id[k])
+	_cache_user.Lock()
+	defer _cache_user.Unlock()
+	return _is_expired(_cache_user.by_id[k])
 }
 
 func Get_user_by_token(k string) *models.User {
-	__cache__user.Lock()
-	defer __cache__user.Unlock()
-	return __is__expired(__cache__user.by__token[k])
+	_cache_user.Lock()
+	defer _cache_user.Unlock()
+	return _is_expired(_cache_user.by_token[k])
 }
 
 func Add_or_update_user(u *models.User) {
@@ -35,27 +37,27 @@ func Add_or_update_user(u *models.User) {
 		helpers.Log(helpers.ERR, "missing token in add or update for cache")
 		return
 	}
-	u.Login__expires = time.Now().Add(time.Hour * 3)
-	__cache__user.Lock()
-	defer __cache__user.Unlock()
-	old_u := __cache__user.by__id[u.Id]
-	__cache__user.by__id[u.Id] = u
+	u.Login_expires = time.Now().Add(time.Hour * 3)
+	_cache_user.Lock()
+	defer _cache_user.Unlock()
+	old_u := _cache_user.by_id[u.Id]
+	_cache_user.by_id[u.Id] = u
 	if nil != old_u {
 		//dont allow multi login
 		//new login voids the old one
-		delete(__cache__user.by__token, old_u.Token)
+		delete(_cache_user.by_token, old_u.Token)
 	}
-	__cache__user.by__token[u.Token] = u
+	_cache_user.by_token[u.Token] = u
 }
 
-func __is__expired(u *models.User) *models.User {
+func _is_expired(u *models.User) *models.User {
 	if nil == u {
 		return u
 	}
-	if u.Login__expires.Unix() < time.Now().Unix() {
+	if u.Login_expires.Unix() < time.Now().Unix() {
 		return nil
 	}
-	u.Login__expires = time.Now().Add(time.Hour * 4)
+	u.Login_expires = time.Now().Add(time.Hour * 4)
 	return u
 }
 
@@ -63,7 +65,7 @@ func Extend_token(u *models.User) {
 	if nil == u {
 		return
 	}
-	u.Login__expires = time.Now().Add(time.Hour * 4)
+	u.Login_expires = time.Now().Add(time.Hour * 4)
 }
 
 //TODO:  add setter so data can be locked
