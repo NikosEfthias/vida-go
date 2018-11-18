c739c9ef474c484b04674e23cbee81178bd3e27a
nikos <nikos@mugsoft.io>
2018-11-18 06:01:00 +0400

    easier to use email preconfigured email func

--- helpers/email.go
+++ helpers/email.go
@@ -3,14 +3,31 @@ package helpers
 import (
 	"net/smtp"
 	"strings"
+
+	"github.com/mugsoft/vida/config"
 )
 
 func SendMail(uname, password, from string, to []string, server string, subject, msg string) error {
+	//{{{
 	headers := "Subject: " + subject + "\r\n\r\n"
 	return smtp.SendMail(server,
 		smtp.PlainAuth("", uname, password, strings.Split(server, ":")[0]),
 		from,
 		to,
 		[]byte(headers+msg),
+	)
+	//}}}
+}
+func SendMailPreconfigured(to []string, subject, msg string) error {
+	return SendMail(
+		//{{{
+		config.Get("APP_EMAIL_ADDR"),
+		config.Get("APP_EMAIL_PASSWD"),
+		config.Get("APP_EMAIL_ADDR"),
+		to,
+		config.Get("SMTP_ADDR"),
+		subject,
+		msg,
+		//}}}
 	)
 }
