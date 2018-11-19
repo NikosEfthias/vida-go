6bce84321680838b4b15a05004997bd0317b0095
nikos <nikos@mugsoft.io>
2018-11-20 03:46:04 +0400

    add guests id as an array to event data

--- models/event.go
+++ models/event.go
@@ -19,20 +19,21 @@ var __col_event = db_get().C(__COL_EVENT_STR)
 // )
 
 type Event struct {
-	Id       string  `json:"id" bson:"id"`
-	Owner    string  `json:"owner" bson:"owner"`
-	Title    string  `json:"title" bson:"title"`
-	Loc      string  `json:"loc" bson:"loc"`
-	Detail   string  `json:"detail" bson:"detail"`
-	MaxGuest int     `json:"max_guest" bson:"max_guest"`
-	MinGuest int     `json:"min_guest" bson:"min_guest"`
-	Cost     float64 `json:"cost" bson:"cost"`
-	Img      string  `json:"img" bson:"img"`
+	Id       string   `json:"id" bson:"id"`
+	Owner    string   `json:"owner" bson:"owner"`
+	Guests   []string `json:"guests" bson:"guests"`
+	Title    string   `json:"title" bson:"title"`
+	Loc      string   `json:"loc" bson:"loc"`
+	Detail   string   `json:"detail" bson:"detail"`
+	MaxGuest int      `json:"max_guest" bson:"max_guest"`
+	MinGuest int      `json:"min_guest" bson:"min_guest"`
+	Cost     float64  `json:"cost" bson:"cost"`
+	Img      string   `json:"img" bson:"img"`
 	// Type      `json:"event_type" bson:"event_type"`
 	StartDate time.Time `json:"start_date" bson:"start_date"`
 	EndDate   time.Time `json:"end_date" bson:"end_date"`
 	Votable   bool      `json:"votable" bson:"votable"`
-	Defaults  `json:"defaults" bson:"defaults"`
+	Defaults
 }
 
 func Event_new(e *Event) error {
