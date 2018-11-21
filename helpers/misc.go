53a5505151d3b76002070cc00bcd377bb7961575
nikos <nikos@mugsoft.io>
2018-11-22 02:38:50 +0400

    check user inputted ids

--- helpers/misc.go
+++ helpers/misc.go
@@ -1,8 +1,13 @@
 package helpers
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
 
+//Check_missing_fields gets string field names and values and checks if they are empty
 func Check_missing_fields(names, values []string) error {
+	//{{{
 	if len(names) != len(values) {
 		return fmt.Errorf("missing fields")
 	}
@@ -11,5 +16,19 @@ func Check_missing_fields(names, values []string) error {
 			return fmt.Errorf("missing field: %s", names[i])
 		}
 	}
-	return nil
+	return nil //}}}
+}
+
+//Check_id_format checks the id format retreived from user
+func Check_id_format(id string) error {
+	//{{{
+	matched, err := regexp.MatchString("^[a-zA-Z0-9]{32}$", id)
+	if nil != err {
+		Log(ERR, "wtf this error is never supposed to be non-nil")
+		return err
+	}
+	if !matched {
+		return fmt.Errorf("invalid id format")
+	}
+	return nil //}}}
 }
