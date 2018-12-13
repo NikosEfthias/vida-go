af8e34a002766b74d0bc448f60f1bd06fd2c798e
nikos <nikos@mugsoft.io>
2018-12-13 23:59:08 +0400

    create folders  if they dont exist

--- helpers/drivers/files/fs/fs.go
+++ helpers/drivers/files/fs/fs.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"gitlab.mugsoft.io/vida/go-api/config"
 	"gitlab.mugsoft.io/vida/go-api/helpers"
@@ -16,6 +17,7 @@ var (
 	E_NOT_FOUND    = os.ErrNotExist
 )
 
+//internals{{{
 //Put file to destination address
 func Put(path string, data []byte) error { //{{{
 	if "" == path {
@@ -24,7 +26,18 @@ func Put(path string, data []byte) error { //{{{
 	if nil == data {
 		return E_INVALID_DATA
 	}
-	f, err := os.OpenFile(config.Get("PUBLIC_FILES_PATH")+path, os.O_CREATE|os.O_WRONLY, 0777)
+	path = config.Get("PUBLIC_FILES_PATH") + path
+	{
+		_path := strings.Split(path, "/")
+		if len(path) > 1 {
+			path := strings.Join(_path[:len(_path)-1], "/") + "/"
+			err := os.MkdirAll(path, 0777)
+			if nil != err {
+				return err
+			}
+		}
+	}
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0777)
 	if nil != err {
 		helpers.Log(helpers.ERR, "invalid path error:", err.Error())
 		return E_INVALID_PATH
@@ -54,4 +67,10 @@ func Get(path string) ([]byte, error) { //{{{
 //Del  file
 func Del(path string) error { //{{{
 	return os.Remove(config.Get("PUBLIC_FILES_PATH") + path)
+} //}}}
+//}}}
+func Put_user_data(user_id string, data []byte) (fname string, err error) { //{{{
+	fname = user_id + "/" + helpers.Unique_id()
+	err = Put(fname, data)
+	return
 } //}}}
